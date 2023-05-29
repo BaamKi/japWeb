b74ed03fe2e2b8bc5ca5a90c3bc9788e2b925315
baamki <sevenbumki@gmail.com>
2023-05-29 16:57:10 +0900

    Initial commit

---
+++ src/main/java/com/beom/japWeb/JapWebApplication.java
@@ -0,0 +1,13 @@
+package com.beom.japWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beom/japWeb/JapWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beom.japWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
